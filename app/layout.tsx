0e871ae3c3c2a260a6109a4c6f749749b5aaa3df
Gabriel Quiroz <gabriel@barbaragoretty.com>
2025-03-03 09:06:06 -0500

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,27 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "VoiGuard - La nueva era de llamadas seguras",
-  description: "VoiGuard transforma cada llamada en una experiencia segura, clara y sin riesgos.",
-    generator: 'v0.dev'
-}
+  title: "VoiGuard - The new era of secure callin",
+  description: "VoiGuard ttransforms every call into a safe, clear and risk-free experience.",
+  generator: "v0.dev",
+  icons: [
+    { rel: "icon", url: "/favicon.png" }, // Favicon por defecto en formato .png
+  ],
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="es">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
-
-
-
-import './globals.css'
